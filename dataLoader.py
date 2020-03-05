8995b0908582969bb96303e283dce0f7353affc7
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-05 13:33:31 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -64,7 +64,7 @@ class ProgramWebDataset(Dataset):
                 if len(title_tokens) + len(dscp_tokens) > 510:
                     continue
 
-                dscp_tokens = dscp_tokens[:100]
+                dscp_tokens = dscp_tokens[:50]
 
                 document.append(" ".join(title_tokens) + " ".join(dscp_tokens))
 
