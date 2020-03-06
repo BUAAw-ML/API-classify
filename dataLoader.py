31d652a3ce016df1205cc5606a6a4f9a6b055986
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-06 16:19:32 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -219,9 +219,9 @@ class ProgramWebDataset(Dataset):
                 if item in self.tfidf_dict:
                     inputs_tfidf[i, j+1] = self.tfidf_dict[item]
 
-        inputs_tfidf[inputs_tfidf>0]=1
-        ids *= inputs_tfidf.long()
-        ids[ids==0]=103
+        # inputs_tfidf[inputs_tfidf>0]=1
+        # ids *= inputs_tfidf.long()
+        # ids[ids==0]=103
 
         return (ids, token_type_ids, attention_mask, inputs_tfidf), tags, dscp
 
