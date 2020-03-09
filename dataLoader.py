11a6be56cfe6dca99f8243826a9b3b142fc3fdd8
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-09 09:19:22 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -84,7 +84,7 @@ class ProgramWebDataset(Dataset):
         ignored_tags = set()  #
 
         for tag in tag_occurance:
-            if tag_occurance[tag] > 300:
+            if tag_occurance[tag] > 200:
                 ignored_tags.add(tag)
 
         for row in buf:
