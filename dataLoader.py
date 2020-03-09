77c019b2fa2c0c0e5a015f8c6cca5bc70bcf51cc
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-09 10:23:49 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -222,7 +222,7 @@ class ProgramWebDataset(Dataset):
     def collate_fn(self, batch):
         result = {}
         # construct input
-        inputs = [e['dscp_ids'] for e in batch]  #e['title_ids'] +
+        inputs = [e['title_ids'] + e['dscp_ids'] for e in batch]  #e['title_ids'] +
 
         lengths = np.array([len(e) for e in inputs])
         max_len = np.max(lengths)
