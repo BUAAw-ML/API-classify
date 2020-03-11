887708226beb96beb1f920d957a8e312e3d93f63
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-11 21:39:46 +0800

    update

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def multiLabel_text_classify():
     train_dataset, val_dataset, encoded_tag, tag_mask = \
         load_dataset('data/ProgrammerWeb/programweb-data.csv', 'data/ProgrammerWeb/domainnet.csv')
 
-    model = gcn_bert(num_classes=len(train_dataset.tag2id), t=0.05, co_occur_mat=train_dataset.co_occur_mat)
+    model = gcn_bert(num_classes=len(train_dataset.tag2id), t=0.2, co_occur_mat=train_dataset.co_occur_mat)
 
     # define loss function (criterion)
     criterion = nn.MultiLabelSoftMarginLoss()
