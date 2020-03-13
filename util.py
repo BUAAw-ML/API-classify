5c7f80cf33aac0a446b3cc5ad751b6cca17f41f4
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-13 18:05:40 +0800

    update

--- util.py
+++ util.py
@@ -215,9 +215,9 @@ def gen_A(num_classes, t, co_occur_mat):
     #np.set_printoptions(threshold=np.inf,suppress=True)
     _adj = co_occur_mat.numpy()
 
-    _adj[_adj < 30] = 0
+    # _adj[_adj < 30] = 0
 
-    _adj = _adj + 1 * np.identity(num_classes, np.int)
+    # _adj = _adj + 1 * np.identity(num_classes, np.int)
     _nums = _adj.diagonal()
     _nums = _nums[:, np.newaxis]
 
@@ -226,10 +226,10 @@ def gen_A(num_classes, t, co_occur_mat):
     print(_adj)
     # exit()
 
-
+    _adj[_adj < t] = 0
     #_adj[_adj >= t] = 1
     _adj = _adj * 0.25 / (_adj.sum(0, keepdims=True) + 1e-6)
-    #_adj = _adj + 1 * np.identity(num_classes, np.int)
+    _adj = _adj + 1 * np.identity(num_classes, np.int)
 
     #     # with open('adj.json', 'w') as f:
     #     #     json.dump(_adj, f)
