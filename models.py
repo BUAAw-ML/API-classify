2cddc2353072b978701f0ef2be5ccdab3ba0a41c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-14 15:49:36 +0800

    update

--- models.py
+++ models.py
@@ -66,13 +66,13 @@ class GCNBert(nn.Module):
         # self.w = nn.Parameter(torch.Tensor(768))
 
         #self.dropout = nn.Dropout(p=0.5)
-        # self.gc1 = GraphConvolution(300, 8000)
-        # self.relu1 = nn.LeakyReLU(0.2)
-        # self.gc2 = GraphConvolution(8000, 768)
-        #
-        # _adj = gen_A(num_classes, t, co_occur_mat)
-        # _adj = torch.FloatTensor(_adj)
-        # self.adj = nn.Parameter(gen_adj(_adj), requires_grad=False)
+        self.gc1 = GraphConvolution(300, 8000)
+        self.relu1 = nn.LeakyReLU(0.2)
+        self.gc2 = GraphConvolution(8000, 768)
+
+        _adj = gen_A(num_classes, t, co_occur_mat)
+        _adj = torch.FloatTensor(_adj)
+        self.adj = nn.Parameter(gen_adj(_adj), requires_grad=False)
         #
         # self.linear1 = nn.Linear(768, 8000)
         # #
