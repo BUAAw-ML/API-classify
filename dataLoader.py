8cb449e5d4bbbde90cf28125d8058f20bc8913fc
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-14 12:58:41 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -341,6 +341,6 @@ def load_dataset(api_csvfile=None, net_csvfile=None):
     print("val_data_size: {}".format(len(val_dataset.data)))
 
     tag_embedding_file = ''
-    #tag_embedding_file = dataset.obtain_tag_embedding()
+    tag_embedding_file = dataset.obtain_tag_embedding()
 
     return train_dataset, val_dataset, encoded_tag, tag_mask, tag_embedding_file
