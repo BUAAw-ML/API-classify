fd570a525ac8c5f22c9407987b318e62dc375c17
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-15 21:17:53 +0800

    update

--- util.py
+++ util.py
@@ -229,7 +229,7 @@ def gen_A(num_classes, t, co_occur_mat):
 
     _adj *= _adj.diagonal() / _nums.sum()
 
-    _adj[_adj < 0.01] = 0
+   # _adj[_adj < 0.01] = 0
     #_adj[_adj >= t] = 1
 
     print(_adj)
