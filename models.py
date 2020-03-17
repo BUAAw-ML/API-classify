8a4f881b2701a89c7d1c8a8a141ed828856c8f06
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-17 09:09:28 +0800

    update

--- models.py
+++ models.py
@@ -60,7 +60,7 @@ class GCNBert(nn.Module):
         # self.w = nn.Parameter(torch.Tensor(768))
 
         #self.dropout = nn.Dropout(p=0.5)
-        self.gc1 = GraphConvolution(108, 8000)
+        self.gc1 = GraphConvolution(768, 8000)
         self.relu1 = nn.LeakyReLU(0.2)
         self.gc2 = GraphConvolution(8000, 768)
 
@@ -106,7 +106,7 @@ class GCNBert(nn.Module):
         # tag_embedding = feats.tolist()
         # tag_embedding = torch.tensor(tag_embedding).cuda(1)
 
-        x = self.gc1(self.adj, self.adj)
+        x = self.gc1(tag_embedding, self.adj)
         x = self.relu1(x)
         x = self.gc2(x, self.adj)
 
