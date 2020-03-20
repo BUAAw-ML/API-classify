39134af8563d96703f39b79ae9e4e41fb6676ce4
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-20 11:19:15 +0800

    update

--- util.py
+++ util.py
@@ -235,7 +235,7 @@ def gen_A(num_classes, t, co_occur_mat):
     #_adj[_adj >= t] = 1
 
     #_adj = _adj * 0.25 / (_adj.sum(0, keepdims=True) + 1e-6)
-    _adj = _adj - 1 * np.identity(num_classes, np.int) + np.identity(num_classes, np.int) * (_nums / _nums.sum(dim=0))
+    _adj = _adj - 1 * np.identity(num_classes, np.int) + np.identity(num_classes, np.int) * (_nums / _nums.sum())
     print(_adj)
 
     #     # with open('adj.json', 'w') as f:
