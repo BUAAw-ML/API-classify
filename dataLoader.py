88517030a46d683278a8fd56fb2eb49b04b5f4c9
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-20 14:03:44 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -216,8 +216,9 @@ class ProgramWebDataset(Dataset):
         # _adj = _adj * 0.25 / (_adj.sum(0, keepdims=True) + 1e-6)
         _adj = _adj + np.identity(tags_num, np.int) * (num >= 1.0 / len(_nums))[:, np.newaxis]
         for i in range(_adj.shape[0]):
-            m = [id2tag[j] for j in range(_adj.shape[1]) if _adj[i][j] > 0]
-            if len(m) > 1:
+            m = [id2tag[i]]
+            m.extend([id2tag[j] for j in range(_adj.shape[1]) if _adj[i][j] > 0])
+            if len(m) > 2:
                 print(m)
 
         exit()
