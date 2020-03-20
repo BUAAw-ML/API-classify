dcaebbe9baef0967b11acc068f4e78d13e28016f
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-20 17:29:31 +0800

    update

--- util.py
+++ util.py
@@ -228,7 +228,7 @@ def gen_A(num_classes, t, co_occur_mat):
 
     #_adj *= _adj.diagonal() / _nums.sum()#* (num >= 1.0 / len(_nums))[:, np.newaxis]
 
-    _adj[_adj < 0.12] = 0
+    _adj[_adj < 0.115] = 0
 
     #print(_adj)
     #_adj[_adj >= t] = 1
