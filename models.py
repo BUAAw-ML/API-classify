219ce0ddec0503ed638e4ea15dfc092afc081081
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-26 11:03:03 +0800

    update

--- models.py
+++ models.py
@@ -120,10 +120,10 @@ class GCNBert(nn.Module):
 
         # sentence_feat = token_feat[:,0,:]
         #
-        # embed = self.bert.get_input_embeddings()
-        # tag_embedding = embed(encoded_tag)
-        # tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
-        #     / torch.sum(tag_mask, dim=1, keepdim=True)
+        embed = self.bert.get_input_embeddings()
+        tag_embedding = embed(encoded_tag)
+        tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
+            / torch.sum(tag_mask, dim=1, keepdim=True)
 
         # with open(tag_embedding_file, 'rb') as fp:
         #     feats = pkl.load(fp)#, encoding='utf-8')
@@ -131,9 +131,9 @@ class GCNBert(nn.Module):
         # tag_embedding2 = torch.tensor(tag_embedding2).cuda(1)
 
 
-        # x = self.gc1(tag_embedding, self.adj)
-        # x = self.relu1(x)
-        # x = self.gc2(x, self.adj)
+        x = self.gc1(tag_embedding, self.adj)
+        x = self.relu1(x)
+        x = self.gc2(x, self.adj)
 
         #
         # # values_memory = self.fc_hallucinator(sentence_feat)
@@ -148,16 +148,15 @@ class GCNBert(nn.Module):
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
 
-        # x = x.transpose(0, 1)
-        # x = torch.matmul(sentence_feat, x)
         #x = x.unsqueeze(0)
         #print(x.shape)
         #x = sentence_feat * x
         # x = self.linear1(sentence_feat)
         # x = self.relu1(x)
-        attention_out = self.linear0(attention_out).squeeze(-1)
-        x = torch.matmul(attention_out, self.adj)
+        sentence_feat = self.linear0(attention_out).squeeze(-1)
 
+        x = x.transpose(0, 1)
+        x = torch.matmul(sentence_feat, x)
         # x = attention_out + x
 
 
