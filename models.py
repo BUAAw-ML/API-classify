ac6e7e7a7c433712ce0188522942086da9e8a48a
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-27 17:25:43 +0800

    update

--- models.py
+++ models.py
@@ -169,10 +169,9 @@ class GCNBert(nn.Module):
         # x = self.relu1(x)
         # x = x.squeeze(-1)
         # x = torch.sum(attention_out, dim=1)
-        print(tag_embedding.unsqueeze(0).shape)
-        print(token_feat.transpose(1, 2).shape)
+
+
         m1 = torch.matmul(tag_embedding, token_feat.transpose(1, 2))
-        print(m1.shape)
         label_att = torch.bmm(m1, token_feat)
 
         weight1 = torch.sigmoid(self.weight1(label_att))
