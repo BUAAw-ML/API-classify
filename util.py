d253f3bfdc62f89547d65e18a10bc14bcc424348
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-28 20:15:25 +0800

    update

--- util.py
+++ util.py
@@ -232,12 +232,12 @@ def gen_A(num_classes, t, co_occur_mat):
 
     _adj[_adj < t] = 0
 
-    _adj[_adj >= t] = 1
+    # _adj[_adj >= t] = 1
    # _adj *= (num < 1.0 / len(_nums))[:, np.newaxis]
 
     _adj = _adj / (_adj.sum(0, keepdims=True)) #* 0.25
 
-    _adj = _adj + np.identity(num_classes, np.int)#
+    # _adj = _adj + np.identity(num_classes, np.int)#
 
 
 
