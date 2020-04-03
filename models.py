67e3d52544b6057d899827ceb8b235fbe774bbaa
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-03 11:06:34 +0800

    update

--- models.py
+++ models.py
@@ -222,11 +222,9 @@ class GCNBert(nn.Module):
         #
         # values_memory = torch.sigmoid(self.fc_hallucinator(self.weight_adj)).squeeze(-1).unsqueeze(0)
 
-        # w1 = torch.sigmoid(self.weight1(self.weight_adj)).squeeze(-1).unsqueeze(0)
-        # pred = self.class_weight * x + attention_out
-        pred = x + attention_out
-        pred = torch.sum(pred, 1) / self.num_classes
-        pred = self.output_layer(pred)
+        w1 = torch.sigmoid(self.weight1(self.weight_adj)).squeeze(-1).unsqueeze(0)
+        pred = w1 * x + attention_out
+        pred = torch.sum(pred, -1)
         # avg_sentence_embeddings = torch.sum(doc, 1) / self.num_classes
         # pred = torch.sigmoid(self.output_layer(avg_sentence_embeddings))
 
