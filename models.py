2e757a1d8b49b14228b98148281cc43f1304df3b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-03 20:17:34 +0800

    update

--- models.py
+++ models.py
@@ -172,7 +172,7 @@ class GCNBert(nn.Module):
         #
         #
         masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
-        attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
+        attention = F.tanh(self.attention(token_feat).transpose(1, 2)).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
         # #
