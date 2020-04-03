e05e82b032b3a65008b55b24c09a849257c63bfc
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-03 21:25:11 +0800

    update

--- models.py
+++ models.py
@@ -108,9 +108,9 @@ class GCNBert(nn.Module):
         # self.fc_selector = nn.Linear(768, num_classes)
 
         self.linear1 = nn.Linear(768 * 2, 500)
-        # self.relu2 = nn.LeakyReLU()
+        self.relu2 = nn.LeakyReLU()
         self.linear2 = nn.Linear(500, 1)
-        self.output_layer = nn.Linear(768, num_classes)
+        # self.output_layer = nn.Linear(768, num_classes)
 
         #self.cosnorm_classifier = CosNorm_Classifier(768, num_classes)
         self.weight1 = torch.nn.Linear(768, 1)
