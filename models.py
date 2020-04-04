2934e4ba31203542ec3b319e0d5852e84537f5f3
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-05 07:45:45 +0800

    update

--- models.py
+++ models.py
@@ -185,7 +185,7 @@ class GCNBert(nn.Module):
         x = self.relu1(x)
         x = self.gc2(x, self.adj)
 
-        x = x.transpose(0, 1)
+        # x = x.transpose(0, 1)
         # x = torch.matmul(sentence_feat, x)
 
         x = torch.mul(sentence_feat.unsqueeze(1), x)
