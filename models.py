7a1e6a13d22e68ec6af7034e30f70d6ef2b65110
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-06 13:15:24 +0800

    update

--- models.py
+++ models.py
@@ -188,14 +188,14 @@ class GCNBert(nn.Module):
         # x = x.transpose(0, 1)
         # x = torch.matmul(sentence_feat, x)
 
-        # x = torch.mul(sentence_feat.unsqueeze(1), x)
+        x = torch.mul(sentence_feat.unsqueeze(1), x)
 
         masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
         # attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
-        attention = (torch.matmul(token_feat, x.transpose(0, 1))).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
+        attention = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
-        attention_out = torch.sum(attention_out,-1)
+        # attention_out = torch.sum(attention_out,-1)
 
 
         # pred = attention_out * x.unsqueeze(0)
@@ -237,9 +237,9 @@ class GCNBert(nn.Module):
 
         # pred = (1-w1) * attention_out + w1 * x
 
-        pred = attention_out# w1 * x +
+        pred = x + attention_out# w1 *
 
-        # pred = torch.sum(pred, -1)
+        pred = torch.sum(pred, -1)
 
 
         # avg_sentence_embeddings = torch.sum(doc, 1) / self.num_classes
