66a5fe1505a6444337140f6de95541e92fe27bc5
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-06 21:05:45 +0800

    update

--- models.py
+++ models.py
@@ -237,11 +237,10 @@ class GCNBert(nn.Module):
 
         # pred = (1-w1) * attention_out + w1 * x
         # pred = torch.cat((attention_out,x),-1)
-        pred = torch.sum(attention_out,1) * self.num_classes
-        pred = self.linear1(pred)
-        # pred = attention_out# w1 *  x +
 
-        # pred = torch.sum(pred, -1)
+        pred = x + attention_out# w1 *
+
+        pred = torch.sum(pred, -1)
 
 
         # avg_sentence_embeddings = torch.sum(doc, 1) / self.num_classes
