36bb1e99ac96f3b3730769b1993d15677a3063f2
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-06 22:01:04 +0800

    update

--- util.py
+++ util.py
@@ -224,7 +224,7 @@ def gen_A(num_classes, t, co_occur_mat):
     _nums = _nums[:, np.newaxis]
     _adj = co_occur_mat / _nums
 
-    t = 0.02
+    t = 0.3
     # _adj = _adj / (_adj.sum(axis=1) + 1e-6)[:, np.newaxis]
     print("the number of directed edges in the graph: {}".format(np.sum(_adj >= t)-num_classes))
 
@@ -240,7 +240,7 @@ def gen_A(num_classes, t, co_occur_mat):
     # origin_adj = origin_adj * (1 - np.identity(num_classes, np.int))
 
     _adj = _adj / (_adj.sum(0, keepdims=True))
-    # _adj = _adj + 1 * np.identity(num_classes, np.int)
+    _adj = _adj + 1 * np.identity(num_classes, np.int)
 
     # _adj = np.round(_adj, 1)
     #
