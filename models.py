d4c73aa184b9914fd84b0afc5d0d923f80c1c4cb
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-06 09:37:10 +0800

    update

--- models.py
+++ models.py
@@ -77,7 +77,7 @@ class GCNBert(nn.Module):
         nn.init.xavier_uniform_(self.attention.weight)
 
         # self.dropout = nn.Dropout(p=0.5)
-        self.gc1 = GraphConvolution(768, 2000)
+        self.gc1 = GraphConvolution(768, 768)
         self.relu1 = nn.LeakyReLU(0.2)
         self.gc2 = GraphConvolution(2000, 768)
 
@@ -182,8 +182,8 @@ class GCNBert(nn.Module):
         # attention_out = torch.sum(attention_out, 1) / self.num_classes
 
         x = self.gc1(tag_embedding, self.adj)
-        x = self.relu1(x)
-        x = self.gc2(x, self.adj)
+        # x = self.relu1(x)
+        # x = self.gc2(x, self.adj)
 
         # x = x.transpose(0, 1)
         # x = torch.matmul(sentence_feat, x)
--- util.py
+++ util.py
@@ -233,7 +233,7 @@ def gen_A(num_classes, t, co_occur_mat):
     # _adj *= (num < 1.0 / len(_nums))[:, np.newaxis]
 
     _adj[_adj < t] = 0
-    # _adj[_adj >= t] = 1
+    _adj[_adj >= t] = 1
 
     origin_adj = co_occur_mat
     # origin_adj = (_adj == 1) * origin_adj
@@ -242,7 +242,7 @@ def gen_A(num_classes, t, co_occur_mat):
     _adj = _adj / (_adj.sum(0, keepdims=True))
     _adj = _adj + 1 * np.identity(num_classes, np.int)
 
-    _adj = np.round(_adj, 1)
+    # _adj = np.round(_adj, 1)
     #
     #np.diag(co_occur_mat.diagonal() / co_occur_mat.diagonal().max())
     print(_adj)
