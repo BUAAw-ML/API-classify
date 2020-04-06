c61da8704466d703db22f6043b30944e4c17cc04
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-06 22:15:36 +0800

    update

--- models.py
+++ models.py
@@ -238,7 +238,7 @@ class GCNBert(nn.Module):
         # pred = (1-w1) * attention_out + w1 * x
         # pred = torch.cat((attention_out,x),-1)
 
-        pred =  attention_out + 5 * x# w1 *
+        pred =  attention_out + 2 * x# w1 *
 
         pred = torch.sum(pred, -1)
 
