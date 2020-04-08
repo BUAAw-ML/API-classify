afbd6fe3a3de3e688b8983b7d9468f5882ec10d7
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-08 13:18:03 +0800

    update

--- models.py
+++ models.py
@@ -73,7 +73,7 @@ class GCNBert(nn.Module):
         # self.linear0 = nn.Linear(768, 768)
         # self.w = nn.Parameter(torch.Tensor(768))
 
-        self.attention = nn.Linear(768, num_classes, bias=False) #num_classes
+        self.attention = nn.Linear(768, 1, bias=False) #num_classes
         nn.init.xavier_uniform_(self.attention.weight)
 
         # self.dropout = nn.Dropout(p=0.5)
@@ -185,17 +185,23 @@ class GCNBert(nn.Module):
         x = self.relu1(x)
         x = self.gc2(x, self.adj)
 
-        x = x.transpose(0, 1)
-        x = torch.matmul(sentence_feat, x)
+        masks = torch.unsqueeze(attention_mask, 1)
+        attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
+        attention = F.softmax(attention, -1)
+        attention_out = attention @ token_feat
+
+        # x = x.transpose(0, 1)
+        # x = torch.matmul(sentence_feat, x)
 
         # x = torch.mul(sentence_feat.unsqueeze(1), x)
+        x = torch.mul(attention_out, x)
 
         masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
         # attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
         attention = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
-        attention_out = torch.sum(attention_out,-1)
+        # attention_out = torch.sum(attention_out,-1)
 
 
         # pred = attention_out * x.unsqueeze(0)
@@ -240,7 +246,7 @@ class GCNBert(nn.Module):
 
         pred = attention_out + x
 
-        # pred = torch.sum(pred, -1)
+        pred = torch.sum(pred, -1)
 
         # avg_sentence_embeddings = torch.sum(pred, 1) #/ self.num_classes
         # pred = torch.matmul(avg_sentence_embeddings, x)
