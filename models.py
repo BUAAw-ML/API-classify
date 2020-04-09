0cb4d674d6f29fc29095cd5a2e7fb407909d3743
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-09 14:24:43 +0800

    update

--- models.py
+++ models.py
@@ -160,10 +160,10 @@ class GCNBert(nn.Module):
         tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
             / torch.sum(tag_mask, dim=1, keepdim=True)
 
-        # with open(tag_embedding_file, 'rb') as fp:
-        #     feats = pkl.load(fp)#, encoding='utf-8')
-        # tag_embedding2 = feats.tolist()
-        # tag_embedding2 = torch.tensor(tag_embedding2).cuda(1)
+        with open(tag_embedding_file, 'rb') as fp:
+            feats = pkl.load(fp)#, encoding='utf-8')
+        tag_embedding2 = feats.tolist()
+        tag_embedding2 = torch.tensor(tag_embedding2).cuda(1)
         #
         # tag_embedding2 = self.linear1(tag_embedding2)
         #
@@ -180,7 +180,7 @@ class GCNBert(nn.Module):
         # attention_out = torch.sum(attention_out, dim=2)
         # attention_out = torch.sum(attention_out, 1) / self.num_classes
 
-        x = self.gc1(tag_embedding, self.adj)
+        x = self.gc1(tag_embedding2, self.adj)
         x = self.relu1(x)
         x = self.gc2(x, self.adj)
 
