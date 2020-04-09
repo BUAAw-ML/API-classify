174699bea28cc025521b875ed97f08634ef1d944
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-09 12:56:11 +0800

    update

--- models.py
+++ models.py
@@ -102,7 +102,7 @@ class GCNBert(nn.Module):
         # print(weight_adj)
         self.weight_adj = torch.FloatTensor(origin_adj).cuda(1)
 
-        self.linear0 = nn.Linear(num_classes, 400)
+        self.linear0 = nn.Linear(num_classes, 1)
 
         # self.fc_hallucinator = nn.Linear(768, num_classes)
         # self.fc_selector = nn.Linear(768, num_classes)
@@ -248,11 +248,9 @@ class GCNBert(nn.Module):
         # pred = torch.matmul(avg_sentence_embeddings, x)
 
         pred = self.linear0(attention_out.transpose(1, 2))
-        pred = self.linear1(pred)
-        pred = self.linear2(pred)
         pred = pred.transpose(1, 2).squeeze(1)
-        # pred = torch.matmul(pred, x)
-        pred = self.output_layer(pred)
+        pred = torch.matmul(pred, x)
+        # pred = self.output_layer(pred)
 
         # pred = torch.matmul(pred, self.adj.transpose(0, 1))
 
