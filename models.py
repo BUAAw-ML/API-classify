270f6b6f61f5ad7f393c6d2346aaaa39dabb2f3b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-11 16:37:15 +0800

    update

--- models.py
+++ models.py
@@ -77,7 +77,7 @@ class GCNBert(nn.Module):
         nn.init.xavier_uniform_(self.attention.weight)
 
         # self.dropout = nn.Dropout(p=0.5)
-        self.gc1 = GraphConvolution(768, 2000)
+        self.gc1 = GraphConvolution(300, 2000)
         self.relu1 = nn.LeakyReLU(0.2)
         self.gc2 = GraphConvolution(2000, 768)
 
@@ -160,10 +160,10 @@ class GCNBert(nn.Module):
         tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
             / torch.sum(tag_mask, dim=1, keepdim=True)
 
-        # with open(tag_embedding_file, 'rb') as fp:
-        #     feats = pkl.load(fp)#, encoding='utf-8')
-        # tag_embedding2 = feats.tolist()
-        # tag_embedding2 = torch.tensor(tag_embedding2).cuda(1)
+        with open(tag_embedding_file, 'rb') as fp:
+            feats = pkl.load(fp)#, encoding='utf-8')
+        tag_embedding2 = feats.tolist()
+        tag_embedding2 = torch.tensor(tag_embedding2).cuda(1)
         #
         # tag_embedding2 = self.linear1(tag_embedding2)
         #
@@ -180,7 +180,7 @@ class GCNBert(nn.Module):
         # attention_out = torch.sum(attention_out, dim=2)
         # attention_out = torch.sum(attention_out, 1) / self.num_classes
 
-        x = self.gc1(tag_embedding, self.adj)
+        x = self.gc1(tag_embedding2, self.adj)
         x = self.relu1(x)
         x = self.gc2(x, self.adj)
         #
@@ -196,7 +196,7 @@ class GCNBert(nn.Module):
         # x = x.unsqueeze(-1)
 
         # attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
-        confidence = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2)
+        confidence = (torch.matmul(token_feat, x.transpose(0, 1))).transpose(1, 2)
 
         masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
         attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
@@ -209,12 +209,12 @@ class GCNBert(nn.Module):
 
         pred = torch.sum(attention_out, -1)
 
-        # masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
-        # attention2 = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
-        # attention2 = F.softmax(attention2, -1)
-        # attention_out2 = attention2 @ token_feat  # N, labels_num, hidden_size
-        # attention_out2 = torch.sum(attention_out2, -1)
-        # pred += attention_out2
+        masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
+        attention2 = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
+        attention2 = F.softmax(attention2, -1)
+        attention_out2 = attention2 @ token_feat  # N, labels_num, hidden_size
+        attention_out2 = torch.sum(attention_out2, -1)
+        pred += attention_out2
 
         # pred = attention_out * x.unsqueeze(0)
 
