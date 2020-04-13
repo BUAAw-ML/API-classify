6c35582dca3fac5ce754c95ab37e41cc4b478bcb
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-13 21:53:01 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -336,7 +336,7 @@ class ProgramWebDataset(Dataset):
         for i, token_list in enumerate(inputs_tokens):
             for j, item in enumerate(token_list):
                 if item in self.tfidf_dict:
-                    inputs_tfidf[i, j+1] = self.tfidf_dict[item]
+                    inputs_tfidf[i, j+1] = 1#self.tfidf_dict[item]
 
         # print(inputs_tokens[0])
         # print(inputs_tfidf[0])
