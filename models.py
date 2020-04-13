fc901828ffd2b201b499293834bb0a4ae25b8bb4
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-13 10:04:00 +0800

    update

--- models.py
+++ models.py
@@ -166,6 +166,12 @@ class GCNBert(nn.Module):
         tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
             / torch.sum(tag_mask, dim=1, keepdim=True)
 
+        title_token_feat = self.bert(title_ids,
+            token_type_ids=title_token_type_ids,
+            attention_mask=title_attention_mask)[0]
+        title_feat = title_token_feat[:, 0, :]
+
+        x = torch.matmul(title_feat, tag_embedding.transpose(0, 1))
 
         # with open(tag_embedding_file, 'rb') as fp:
         #     feats = pkl.load(fp)#, encoding='utf-8')
@@ -226,7 +232,7 @@ class GCNBert(nn.Module):
 
         # x = torch.cat((x, attention_out), 2)
 
-        pred = torch.sum(attention_out, -1)
+        pred = torch.sum(attention_out, -1) + x
 
         #x = x.unsqueeze(0)
         #print(x.shape)
