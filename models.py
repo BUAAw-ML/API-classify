0e0d67d6944b1313442133c2f39de812556eb567
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-13 21:48:28 +0800

    update

--- models.py
+++ models.py
@@ -200,12 +200,8 @@ class GCNBert(nn.Module):
         #
         # x = torch.mul(sentence_feat.unsqueeze(1), x)
 
-        print(inputs_tfidf.unsqueeze(-1).shape)
-
         # tag_embedding = torch.matmul(self.adj, tag_embedding)
-        masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
-        print(masks.shape)
-        exit()
+        masks = torch.unsqueeze(attention_mask * inputs_tfidf, 1)  # N, 1, L
 
         # attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
         attention = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))
