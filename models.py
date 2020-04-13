f3d666babab3732da93868f7ff6368d9ca9b146a
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-13 12:49:12 +0800

    update

--- models.py
+++ models.py
@@ -202,7 +202,7 @@ class GCNBert(nn.Module):
 
 
 
-        tag_embedding = torch.matmul(self.adj, tag_embedding)
+        # tag_embedding = torch.matmul(self.adj, tag_embedding)
         masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L
 
         # attention = self.attention(token_feat).transpose(1, 2).masked_fill(1 - masks.byte(), torch.tensor(-np.inf))  # N, labels_num, L
@@ -249,7 +249,7 @@ class GCNBert(nn.Module):
 
         # w1 = torch.sigmoid(self.weight1(self.weight_adj)).squeeze(-1).unsqueeze(0)
 
-        pred = attention_out #+ w1 * x
+        pred = attention_out + x
 
 
         # pred = 0.5 * torch.sigmoid(attention_out) + 0.5 * torch.sigmoid(x)
