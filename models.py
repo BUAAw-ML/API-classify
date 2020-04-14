4b9acbfbe7e19d9670d4db48e8a6e4dcc8cf08b7
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-14 11:04:25 +0800

    update

--- models.py
+++ models.py
@@ -216,7 +216,7 @@ class GCNBert(nn.Module):
 
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
 
-        attention_out = torch.sum(attention_out, -1) #/ torch.sum(attention_mask, dim=1, keepdim=True)
+        attention_out = torch.sum(attention_out, -1) / torch.sum(masks.squeeze(1), dim=1, keepdim=True)
 
         # self.memory = torch.mean(attention_out, 0).clone()
 
