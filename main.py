dd76e54d9474e2871b4838c06a1a79aeb8388885
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-17 09:47:21 +0800

    update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ parser.add_argument('-b', '--batch-size', default=16, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
-parser.add_argument('--lrp', '--learning-rate-pretrained', default=0.01, type=float,
+parser.add_argument('--lrp', '--learning-rate-pretrained', default=0.001, type=float,
                     metavar='LR', help='learning rate for pre-trained layers')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
