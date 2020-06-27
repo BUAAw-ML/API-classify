b772e8f85fb8aa826d8db61830e444468aed15a4
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-06-27 15:46:36 +0800

    update

--- models.py
+++ models.py
@@ -130,7 +130,7 @@ class GCNBert(nn.Module):
         self.weight0 = torch.nn.Linear(768, 1)
 
         self.weight3 = Parameter(torch.Tensor(13, 1))
-        self.weight3.data.uniform_(0, 1)
+        # self.weight3.data.uniform_(0, 1)
 
         # self.memory = Parameter(torch.Tensor(num_classes, 768), requires_grad=False).cuda(0)
 
