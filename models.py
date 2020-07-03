5b75c2ab265b3881718fdbc514f098afe2124f6d
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-07-03 17:01:04 +0800

    update

--- models.py
+++ models.py
@@ -86,9 +86,9 @@ class GCNBert(nn.Module):
         nn.init.xavier_uniform_(self.attention.weight)
 
         # self.dropout = nn.Dropout(p=0.5)
-        self.gc1 = GraphConvolution(768, 2000)
+        self.gc1 = GraphConvolution(768, 1000)
         self.relu1 = nn.LeakyReLU(0.2)
-        self.gc2 = GraphConvolution(2000, 768)
+        self.gc2 = GraphConvolution(1000, 768)
 
         _adj, origin_adj = gen_A(num_classes, t, co_occur_mat)
 
