e5eeb3de12fbd309e3339496a100247043c2e31a
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-07-08 15:09:45 +0800

    update

--- models.py
+++ models.py
@@ -260,7 +260,7 @@ class GCNBert(nn.Module):
         # x = torch.cat((x, attention_out), 2)
 
         attention_out = attention_out * self.class_weight
-        pred = torch.sum(attention_out, -1)
+        pred = torch.sum(attention_out, -1) + x
 
         # self.memory = torch.mean(attention_out, 0).clone()
 
