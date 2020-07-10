01cad650691f0fc4eab2c4392fbae34591c1adb5
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-07-10 18:32:25 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -449,7 +449,7 @@ def load_dataset(api_csvfile=None, net_csvfile=None):
         train_dataset = dataset
         val_dataset = copy.copy(dataset)
         ind = np.random.RandomState(seed=10).permutation(len(data))
-        split = int(len(data) * 0.8)
+        split = int(len(data) * 0.85)
         train_dataset.data = data[ind[:split]].tolist()
         val_dataset.data = data[ind[split:]].tolist()
 
