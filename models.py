5f4480165bb305962ce0de7d952109d8336add9e
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-07-10 08:25:37 +0800

    update

--- models.py
+++ models.py
@@ -86,9 +86,9 @@ class GCNBert(nn.Module):
         nn.init.xavier_uniform_(self.attention.weight)
 
         # self.dropout = nn.Dropout(p=0.5)
-        self.gc1 = GraphConvolution(768, 1500)
+        self.gc1 = GraphConvolution(768, 2000)
         self.relu1 = nn.LeakyReLU(0.2)
-        self.gc2 = GraphConvolution(1500, 2000)
+        self.gc2 = GraphConvolution(2000, 768)
         self.relu2 = nn.LeakyReLU(0.2)
         self.gc3 = GraphConvolution(2000, 768)
 
@@ -232,8 +232,7 @@ class GCNBert(nn.Module):
         x = self.gc1(tag_embedding, self.adj)
         x = self.relu1(x)
         x = self.gc2(x, self.adj)
-        x = self.relu2(x)
-        x = self.gc3(x, self.adj)
+
         # # #
         x = x.transpose(0, 1)
         x = torch.matmul(sentence_feat, x)
@@ -266,7 +265,7 @@ class GCNBert(nn.Module):
         # x = torch.cat((x, attention_out), 2)
 
         attention_out = attention_out * self.class_weight
-        pred = torch.sum(attention_out, -1)
+        pred = torch.sum(attention_out, -1) + x
 
         # self.memory = torch.mean(attention_out, 0).clone()
 
