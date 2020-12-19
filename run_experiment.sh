e0e79da12998bf842d0d358b14ba626dd895eddb
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-19 15:57:50 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,7 +4,28 @@
 #EN='02'  #experiment_no
 
 python main.py \
-        --experiment_no='tagFrequence<200_Test'  \
+        --experiment_no='tagFrequence<300'  \
+        --epochs=50 \
+        --epoch_step=30 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='All' \
+        --data_path='../datasets/ProgrammerWeb/programweb-data.csv' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='skip' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=300 \
+        --intanceNum_limit=99999 \
+        --data_split=0.05  \
+        --test_description=''  \
+
+python main.py \
+        --experiment_no='tagFrequence<300'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -19,7 +40,7 @@ python main.py \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='skip' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=99999 \
+        --max_tagFrequence=300 \
         --intanceNum_limit=99999 \
         --data_split=0.05  \
         --test_description=''  \
