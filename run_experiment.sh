678dd7d671cc1cab0630a7723da78d2735b04e22
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-21 23:05:19 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -21,7 +21,7 @@ python main.py \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
-        --data_split=0.15  \
+        --data_split=0.35  \
         --test_description=''  \
 
 python main.py \
@@ -42,7 +42,7 @@ python main.py \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
-        --data_split=0.15  \
+        --data_split=0.35  \
         --test_description=''  \
 
 
