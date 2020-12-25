5a9d58483d3f1930a5ddb9335565a2e8f701d647
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-25 09:15:13 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -19,7 +19,7 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='skip' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=200 \
+        --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
         --data_split=1  \
         --test_description=''  \
