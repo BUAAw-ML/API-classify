5e1c020e6b1cec1f0fef92492774b0c591ea25a9
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-26 21:44:19 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,29 +3,50 @@
 
 #EN='02'  #experiment_no
 
+#python main.py \
+#        --experiment_no='min_tagFrequence=100'  \
+#        --epochs=50 \
+#        --epoch_step=30 \
+#        --device_ids=0 \
+#        --batch-size=4 \
+#        --G-lr=0.001 \
+#        --D-lr=0.1 \
+#        --B-lr=0.01 \
+#        --save_model_path='./checkpoint' \
+#        --data_type='All' \
+#        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
+#        --use_previousData=0 \
+#        --method='MultiLabelMAP' \
+#        --overlength_handle='truncation' \
+#        --min_tagFrequence=100  \
+#        --max_tagFrequence=99999 \
+#        --intanceNum_limit=99999 \
+#        --data_split=0.05 \
+#        --test_description=''  \
+
 python main.py \
-        --experiment_no='min_tagFrequence=100'  \
-        --epochs=50 \
-        --epoch_step=30 \
+        --experiment_no='Test_3000'  \
+        --epochs=100 \
+        --epoch_step=90 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.01 \
+        --B-lr=0.001 \
         --save_model_path='./checkpoint' \
         --data_type='All' \
         --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
         --use_previousData=0 \
-        --method='MultiLabelMAP' \
+        --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
+        --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
         --data_split=0.05 \
         --test_description=''  \
 
 python main.py \
-        --experiment_no='min_tagFrequence=100'  \
+        --experiment_no='Test_3000'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -39,36 +60,14 @@ python main.py \
         --use_previousData=0 \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
-        --max_tagFrequence=99999 \
-        --intanceNum_limit=99999 \
-        --data_split=0.05 \
-        --test_description=''  \
-
-
-python main.py \
-        --experiment_no='min_tagFrequence=100'  \
-        --epochs=50 \
-        --epoch_step=30 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='All' \
-        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
+        --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
         --data_split=0.1 \
         --test_description=''  \
 
 python main.py \
-        --experiment_no='min_tagFrequence=100'  \
+        --experiment_no='Test_3000'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -82,37 +81,14 @@ python main.py \
         --use_previousData=0 \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
-        --max_tagFrequence=99999 \
-        --intanceNum_limit=99999 \
-        --data_split=0.1 \
-        --test_description=''  \
-
-
-
-python main.py \
-        --experiment_no='min_tagFrequence=100'  \
-        --epochs=50 \
-        --epoch_step=30 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='All' \
-        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
+        --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
         --data_split=0.2 \
         --test_description=''  \
 
 python main.py \
-        --experiment_no='min_tagFrequence=100'  \
+        --experiment_no='Test_3000'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -126,50 +102,7 @@ python main.py \
         --use_previousData=0 \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
-        --max_tagFrequence=99999 \
-        --intanceNum_limit=99999 \
-        --data_split=0.2 \
-        --test_description=''  \
-
-
-python main.py \
-        --experiment_no='min_tagFrequence=100'  \
-        --epochs=50 \
-        --epoch_step=30 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='All' \
-        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
-        --max_tagFrequence=99999 \
-        --intanceNum_limit=99999 \
-        --data_split=0.3 \
-        --test_description=''  \
-
-python main.py \
-        --experiment_no='min_tagFrequence=100'  \
-        --epochs=100 \
-        --epoch_step=90 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.001 \
-        --save_model_path='./checkpoint' \
-        --data_type='All' \
-        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
-        --use_previousData=0 \
-        --method='semiGAN_MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=100  \
+        --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
         --data_split=0.3 \
