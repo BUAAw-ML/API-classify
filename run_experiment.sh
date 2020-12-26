32fdbeb5b4c42aaa8ba2ca74c670c5798c6e189d
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-26 13:13:00 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -21,7 +21,7 @@ python main.py \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
-        --data_split=0.1 \
+        --data_split=0.2 \
         --test_description=''  \
 
 python main.py \
@@ -42,7 +42,49 @@ python main.py \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
-        --data_split=0.1 \
+        --data_split=0.2 \
+        --test_description=''  \
+
+python main.py \
+        --experiment_no='Test'  \
+        --epochs=50 \
+        --epoch_step=30 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='All' \
+        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=99999 \
+        --intanceNum_limit=99999 \
+        --data_split=0.3 \
+        --test_description=''  \
+
+python main.py \
+        --experiment_no='Test'  \
+        --epochs=100 \
+        --epoch_step=90 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='All' \
+        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=99999 \
+        --intanceNum_limit=99999 \
+        --data_split=0.3 \
         --test_description=''  \
 
 #方法、epoch_step
