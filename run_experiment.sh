b12d26d7fc76273875018b21f83efb80c9f6f51e
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-27 08:40:44 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,20 +3,41 @@
 
 #EN='02'  #experiment_no
 
+#python main.py \
+#        --experiment_no='Test_500'  \
+#        --epochs=50 \
+#        --epoch_step=30 \
+#        --device_ids=0 \
+#        --batch-size=4 \
+#        --G-lr=0.001 \
+#        --D-lr=0.1 \
+#        --B-lr=0.01 \
+#        --save_model_path='./checkpoint' \
+#        --data_type='All' \
+#        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
+#        --use_previousData=0 \
+#        --method='MultiLabelMAP' \
+#        --overlength_handle='truncation' \
+#        --min_tagFrequence=0  \
+#        --max_tagFrequence=99999 \
+#        --intanceNum_limit=99999 \
+#        --data_split=0.05 \
+#        --test_description=''  \
+
 python main.py \
         --experiment_no='Test_500'  \
-        --epochs=50 \
-        --epoch_step=30 \
+        --epochs=100 \
+        --epoch_step=90 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.01 \
+        --B-lr=0.001 \
         --save_model_path='./checkpoint' \
         --data_type='All' \
         --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
         --use_previousData=0 \
-        --method='MultiLabelMAP' \
+        --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
@@ -42,10 +63,29 @@ python main.py \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999 \
         --intanceNum_limit=99999 \
-        --data_split=0.05 \
+        --data_split=0.1\
         --test_description=''  \
 
-
+python main.py \
+        --experiment_no='Test_500'  \
+        --epochs=100 \
+        --epoch_step=90 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='All' \
+        --data_path='../datasets/ProgrammerWeb/Programweb_APIs.csv' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=99999 \
+        --intanceNum_limit=99999 \
+        --data_split=0.2 \
+        --test_description=''  \
 
 
 
