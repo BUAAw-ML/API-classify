20ad0d9d438894b406aa5eb5da1356284c12e59f
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-03 19:15:03 +0800

    update

--- util.py
+++ util.py
@@ -192,7 +192,10 @@ class AveragePrecisionMeter(object):
             Nc[k] = np.sum(targets * (scores >= 0.5))
 
             N[k] = np.sum(scores)
+            print(N[k])
             Na[k] = np.sum(targets * (scores >= 0.5)) + np.sum((1 - targets) * (scores < 0.5))
+            print(Na[k])
+            print("----")
 
         # Np[Np == 0] = 1
         # OP = np.sum(Nc) / np.sum(Np + 1e-5)
