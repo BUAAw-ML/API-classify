c638d3718f3ec236e6bcc142e43150feffc45e44
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-05 16:51:22 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -346,14 +346,19 @@ class dataEngine(Dataset):
 
         print('Total number of tags: {}'.format(len(tag_occurance)))
         tags = sorted(tag_occurance.items(), key=lambda x: x[1], reverse=True)
-        print(tags[:self.data_config['max_tagFrequence']])
+
 
 
         ignored_tags = set(['Tools','Applications','Other', 'API', 'Software-as-a-Service','Platform-as-a-Service',
-        'Data-as-a-Service','Widgets'])  #
+        'Data-as-a-Service','Widgets'])
+
+        for item in ignored_tags:
+            tags.pop(item)
+
+        print(tags)
+
         for item in tags[self.data_config['min_tagFrequence']:self.data_config['max_tagFrequence']]:
-            if item[0] not in ignored_tags:
-                self.use_tags.add(item[0])
+            self.use_tags.add(item[0])
 
 
         # for tag in tag_occurance:
