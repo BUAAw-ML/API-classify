c8ba7b48443affeb5ab859b7591ad4e1300e331e
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-06 10:45:55 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -417,6 +417,6 @@ class dataEngine(Dataset):
                 })
 
         print("The number of tags for training: {}".format(len(self.tag2id)))
-        # print(self.id2tag)
+        print(self.id2tag)
         print("taglen: {}".format(taglen/item))
         return data
