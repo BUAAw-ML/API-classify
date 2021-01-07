aa5b887dca30a7b790830632769c62b03940f22e
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-07 13:03:29 +0800

    update

--- models.py
+++ models.py
@@ -48,11 +48,11 @@ class MABert(nn.Module):
 
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
-        attention_out = attention_out * self.class_weight
-        attention_out = torch.sum(attention_out, -1)
-        # attention_out = self.Linear1(attention_out)#.squeeze(-1)
-        # attention_out = self.act(attention_out)
-        # attention_out = self.Linear2(attention_out).squeeze(-1)
+        # attention_out = attention_out * self.class_weight
+        # attention_out = torch.sum(attention_out, -1)
+        attention_out = self.Linear1(attention_out)#.squeeze(-1)
+        attention_out = self.act(attention_out)
+        attention_out = self.Linear2(attention_out).squeeze(-1)
         logit = torch.sigmoid(attention_out)
 
         feat = feat * self.class_weight
