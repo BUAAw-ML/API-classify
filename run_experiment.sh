18e31075932b34a09692bc1b6003c83d7fc808e3
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-07 18:13:50 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -27,7 +27,7 @@
 #        --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyc_linear_filter_0<tag<50'  \
+        --experiment_no='zyz_linear_filter_0<tag<150'  \
         --epochs=50 \
         --epoch_step=40 \
         --device_ids=0 \
@@ -42,9 +42,75 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
+        --max_tagFrequence=150 \
         --intanceNum_limit=99999 \
         --data_split=200 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='zyz_linear_filter_0<tag<150'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=150 \
+        --intanceNum_limit=99999 \
+        --data_split=400 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='zyz_linear_filter_0<tag<150'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=150 \
+        --intanceNum_limit=99999 \
+        --data_split=1600 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='zyz_linear_filter_0<tag<150'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=150 \
+        --intanceNum_limit=99999 \
+        --data_split=6400 \
         --test_description=''  \
         --model_backend='pytorch'
 
