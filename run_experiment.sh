a7b3353bb5d7ba97c23bd661b8114d8eaaa7a998
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-07 21:12:03 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,9 +3,8 @@
 
 #EN='02'  #experiment_no
 
-
 python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<50'  \
+        --experiment_no='wqb_filter_0<tag<200'  \
         --epochs=50 \
         --epoch_step=40 \
         --device_ids=0 \
@@ -20,102 +19,36 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
+        --max_tagFrequence=200 \
         --intanceNum_limit=99999 \
         --data_split=200 \
         --test_description=''  \
         --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<50'  \
-        --epochs=50 \
-        --epoch_step=40 \
+        --experiment_no='wqb_filter_0<tag<200'  \
+        --epochs=100 \
+        --epoch_step=90 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.01 \
+        --B-lr=0.001 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTest' \
         --data_path='../datasets/ProgrammerWeb/' \
         --use_previousData=0 \
-        --method='MultiLabelMAP' \
+        --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
-        --intanceNum_limit=99999 \
-        --data_split=400 \
-        --test_description=''  \
-        --model_backend='pytorch'
-
-python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<50'  \
-        --epochs=50 \
-        --epoch_step=40 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTest' \
-        --data_path='../datasets/ProgrammerWeb/' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
-        --intanceNum_limit=99999 \
-        --data_split=1600 \
-        --test_description=''  \
-        --model_backend='pytorch'
-
-python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<50'  \
-        --epochs=50 \
-        --epoch_step=40 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTest' \
-        --data_path='../datasets/ProgrammerWeb/' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
-        --intanceNum_limit=99999 \
-        --data_split=6400 \
-        --test_description=''  \
-        --model_backend='pytorch'
-
-python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<100'  \
-        --epochs=50 \
-        --epoch_step=40 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTest' \
-        --data_path='../datasets/ProgrammerWeb/' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=100 \
+        --max_tagFrequence=200 \
         --intanceNum_limit=99999 \
         --data_split=200 \
         --test_description=''  \
         --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<100'  \
+        --experiment_no='wqb_filter_0<tag<200'  \
         --epochs=50 \
         --epoch_step=40 \
         --device_ids=0 \
@@ -130,15 +63,36 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=100 \
+        --max_tagFrequence=200 \
         --intanceNum_limit=99999 \
         --data_split=400 \
         --test_description=''  \
         --model_backend='pytorch'
 
+python main.py \
+        --experiment_no='wqb_filter_0<tag<200'  \
+        --epochs=100 \
+        --epoch_step=90 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=200 \
+        --intanceNum_limit=99999 \
+        --data_split=400 \
+        --test_description=''  \
+        --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<100'  \
+        --experiment_no='wqb_filter_0<tag<200'  \
         --epochs=50 \
         --epoch_step=40 \
         --device_ids=0 \
@@ -153,14 +107,36 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=100 \
+        --max_tagFrequence=200 \
         --intanceNum_limit=99999 \
         --data_split=1600 \
         --test_description=''  \
         --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyc_sum_filter_0<tag<100'  \
+        --experiment_no='wqb_filter_0<tag<200'  \
+        --epochs=100 \
+        --epoch_step=90 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=200 \
+        --intanceNum_limit=99999 \
+        --data_split=1600 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='wqb_filter_0<tag<200'  \
         --epochs=50 \
         --epoch_step=40 \
         --device_ids=0 \
@@ -175,11 +151,34 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=100 \
+        --max_tagFrequence=200 \
         --intanceNum_limit=99999 \
         --data_split=6400 \
         --test_description=''  \
         --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='wqb_filter_0<tag<200'  \
+        --epochs=100 \
+        --epoch_step=90 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=200 \
+        --intanceNum_limit=99999 \
+        --data_split=6400 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
 #方法、epoch_step
 
 #batch-size：1，4，8，16
