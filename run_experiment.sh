de5747c234245b44dd627c611a19d7018fd1868c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-12 12:21:35 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -23,7 +23,7 @@ python main.py \
         --min_tagFrequence=0  \
         --max_tagFrequence=50 \
         --intanceNum_limit=99999 \
-        --data_split=200 \
+        --data_split=400 \
         --test_description=''  \
         --model_backend='pytorch'
 
