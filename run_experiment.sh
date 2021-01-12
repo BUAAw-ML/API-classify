414165d92bfac051e9713a25c0c51f93691bc6cf
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-12 08:17:22 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -6,29 +6,29 @@
 
 
 python main.py \
-        --experiment_no='zyz_combineLoss_0<tag<50'  \
-        --epochs=100 \
-        --epoch_step=90 \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
+        --epochs=50 \
+        --epoch_step=40 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.001 \
+        --B-lr=0.01 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTest' \
         --data_path='../datasets/ProgrammerWeb/' \
         --use_previousData=0 \
-        --method='semiGAN_MultiLabelMAP' \
+        --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
+        --max_tagFrequence=100 \
         --intanceNum_limit=99999 \
         --data_split=200 \
         --test_description=''  \
         --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyz_combineLoss_0<tag<50'  \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -43,14 +43,37 @@ python main.py \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
+        --max_tagFrequence=100 \
+        --intanceNum_limit=99999 \
+        --data_split=200 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+
+python main.py \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=100 \
         --intanceNum_limit=99999 \
         --data_split=400 \
         --test_description=''  \
         --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyz_combineLoss_0<tag<50'  \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -65,14 +88,80 @@ python main.py \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
+        --max_tagFrequence=100 \
+        --intanceNum_limit=99999 \
+        --data_split=400 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=100 \
+        --intanceNum_limit=99999 \
+        --data_split=800 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
+        --epochs=100 \
+        --epoch_step=90 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=100 \
+        --intanceNum_limit=99999 \
+        --data_split=800 \
+        --test_description=''  \
+        --model_backend='pytorch'
+
+python main.py \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb/' \
+        --use_previousData=0 \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=100 \
         --intanceNum_limit=99999 \
         --data_split=1600 \
         --test_description=''  \
         --model_backend='pytorch'
 
 python main.py \
-        --experiment_no='zyz_combineLoss_0<tag<50'  \
+        --experiment_no='zyz_DLunsupervised2_0<tag<100'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -87,9 +176,9 @@ python main.py \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50 \
+        --max_tagFrequence=100 \
         --intanceNum_limit=99999 \
-        --data_split=6400 \
+        --data_split=1600 \
         --test_description=''  \
         --model_backend='pytorch'
 
