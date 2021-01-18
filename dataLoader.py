0879c14ce540da5d4867d02a6f08352c8b089bcf
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-18 21:11:50 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -395,8 +395,9 @@ class dataEngine(Dataset):
 
         print('Total number of tags: {}'.format(len(tag_occurance)))
         tags = sorted(tag_occurance.items(), key=lambda x: x[1], reverse=True)
-        # print(tags)
+        print(tags)
         print(tags[:self.data_config['max_tagFrequence']])
+        exit()
 
         for item in tags[self.data_config['min_tagFrequence']:self.data_config['max_tagFrequence']]:
             self.use_tags[item[0]] = int(item[1])
